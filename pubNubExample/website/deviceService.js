5fec3822c57c4a5458a14acc803497d314680080
Guy Segal <guy@soluto.com>
2016-06-23 11:18:22 +0300

    improved pubnub example

--- pubNubExample/website/deviceService.js
+++ pubNubExample/website/deviceService.js
@@ -1,12 +1,8 @@
 var CongoPubNubInvoker = require('congo-pubnub-js').CongoPubNubInvoker;
 var congoProxy = require('congo-proxy');
 
-var pnConfiguration = {
-    publishKey: "pub-c-4ccbfeff-cdb1-497a-a00c-2f01f367ff64",
-    subscribeKey: "sub-c-ba17d4ee-2f0d-11e6-9327-02ee2ddab7fe"
-};
-
-var invoker = new CongoPubNubInvoker(pnConfiguration, "request_b4d5b93c-9a0a-4386-b1b4-0e7d6be4e7d2", "response_b4d5b93c-9a0a-4386-b1b4-0e7d6be4e7d2");
+var pubnubConfiguration = {publishKey: "pub-c-4ccbfeff-cdb1-497a-a00c-2f01f367ff64",  subscribeKey: "sub-c-ba17d4ee-2f0d-11e6-9327-02ee2ddab7fe"};
+var invoker = new CongoPubNubInvoker(pubnubConfiguration, "request_b4d5b93c-9a0a-4386-b1b4-0e7d6be4e7d2", "response_b4d5b93c-9a0a-4386-b1b4-0e7d6be4e7d2");
 
 var proxy = congoProxy("someService", invoker);
 proxy.registerObservable("getTime");
--- pubNubExample/website/main.js
+++ pubNubExample/website/main.js
@@ -10,72 +10,29 @@ import deviceService from './deviceService';
 // </editor-fold>
 
 //<editor-fold desc="Time">
-var startClockClicks = DOM.click(document.querySelector("#startTime"));
-var stopClockClicks = DOM.click(document.querySelector("#stopTime"));
+
+var startTimeButtonClicks = DOM.click(document.querySelector("#startTimeButton"));
+var stopTimeButtonClicks = DOM.click(document.querySelector("#stopTimeButton"));
 var timeView = document.querySelector("#timeView");
 
 //const getTime = () => Observable.interval(1000).map(() => new Date());
 const getTime = () => deviceService.getTime();
 
-//startClockClicks
-//    .flatMap(() => getTime())
-//    .takeUntil(stopClockClicks)
-//    .map(time => timeView.innerText = time)
-//    .repeat()
-//    .subscribe();
-
-startClockClicks
-    .flatMap(() => getTime().takeUntil(stopClockClicks))
+startTimeButtonClicks
+    .flatMap(() => getTime().takeUntil(stopTimeButtonClicks))
     .map(time => timeView.innerText = time)
     .subscribe();
 // </editor-fold>
 
-//<editor-fold desc="Process Count">
-
-var processCountStartClicks = DOM.click(document.querySelector("#startProcessCount"));
-var processCountStopClicks = DOM.click(document.querySelector("#stopProcessCount"));
-var processCountView = document.querySelector("#processCount");
-
-const getProcessCount = () => deviceService.getRunningProcessesCount().retryUntilResponse(5000, 10);
-
-processCountStartClicks
-    .flatMap(() => getProcessCount())
-    .distinctUntilChanged()
-    .takeUntil(processCountStopClicks)
-    .map(processCount => processCountView.innerHTML = processCount)
-    .repeat()
-    .subscribe();
-
-// </editor-fold>
-
 //<editor-fold desc="Logs">
 
-//var logStartClicks = DOM.click(document.querySelector("#startLogs"));
-//var logStopClicks = DOM.click(document.querySelector("#stopLogs"));
-//var logView = document.querySelector("#logsView");
-//
-//const getLogs = () => deviceService.getLogs();
-//
-//const buildLogsHtml = (logs)=>logs.map(log=> `<li>${log}</li>`).join("");
-//
-//var logs = logStartClicks
-//    .flatMap(() => getLogs().takeUntil(logStopClicks))
-//    .distinct()
-//    .scan((acc, next)=> [next, ...acc], []);
-//
-//logs.map(logs=> buildLogsHtml(logs))
-//    .subscribe(html => logView.innerHTML = html);
-//</editor-fold>
-
-//<editor-fold desc="Logs with Filter">
-
-var logStartClicks = DOM.click(document.querySelector("#startLogs"));
-var logStopClicks = DOM.click(document.querySelector("#stopLogs"));
+var logStartClicks = DOM.click(document.querySelector("#startLogsButton"));
+var logStopClicks = DOM.click(document.querySelector("#stopLogsButton"));
 var logView = document.querySelector("#logsView");
 
 const getLogs = () => deviceService.getLogs();
 
-const buildLogsHtml = (logs)=>logs.map(log=> `<li>${log}</li>`).join("");
+const buildLogsHtml = (logs)=> logs.map(log=> `<li>${log}</li>`).join("");
 
 var logs = logStartClicks
     .flatMap(() => getLogs().takeUntil(logStopClicks))
@@ -83,6 +40,8 @@ var logs = logStartClicks
     .scan((acc, next)=> [next, ...acc], []);
 
 var logFiltersChange = DOM.keyup(document.querySelector("#logsFilter"));
+
+//<editor-fold desc="With Filter">
 
 var logsFilter = logFiltersChange
     .map(e=> e.target.value)
@@ -94,4 +53,7 @@ Observable
     .combineLatest(logs,logsFilter, (logs, logsFilter)=> logs.filter(log=> log.includes(logsFilter)))
     .map(logs=> buildLogsHtml(logs))
     .subscribe(html => logView.innerHTML = html);
+
+//</editor-fold>
+
  //</editor-fold>
